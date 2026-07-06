12fedb322b5ba53b18a44c4f208e2ef7790a39df
SameerGangurde71 <sameergangurde71@gmail.com>
2026-07-06 15:28:28 +0530

    fix: update PORT assignment to use logical OR instead of nullish coalescing

--- Backend/index.js
+++ Backend/index.js
@@ -6,7 +6,7 @@ import userRouter from './routes/user.routes.js'
 import {authenticationMiddleware} from './middlewares/auth.middleware.js'
 import urlRouter from './routes/url.routes.js';
 const app = express()
-const PORT = process.env.PORT ?? 8000;
+const PORT = process.env.PORT || 8000;
 app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 
 app.use(express.json());
