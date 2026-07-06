bf0a67c4480c185dad8dfb1744aa5860ba9fc781
SameerGangurde71 <sameergangurde71@gmail.com>
2026-07-06 15:54:54 +0530

    fix: update CORS configuration to allow requests from production URL

--- Backend/index.js
+++ Backend/index.js
@@ -7,7 +7,9 @@ import {authenticationMiddleware} from './middlewares/auth.middleware.js'
 import urlRouter from './routes/url.routes.js';
 const app = express()
 const PORT = process.env.PORT || 8000;
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: ['http://localhost:5173', 
+        'https://snip-url-shortener-f1rmesxru-sameer71.vercel.app'],
+    credentials: true }));
 
 app.use(express.json());
 app.use(authenticationMiddleware);
